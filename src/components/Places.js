f9481e08044c39f590d05c2efe397d2d969c4c69
Dioni Mercado <me@dionimercado.com>
2018-11-24 12:07:09 -0400

    Fixed scroll on Places list

--- src/components/Places.js
+++ src/components/Places.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default ({ places }) => {
   return (
-    <div>
+    <div style={{ height: "calc(100vh - 56px)", overflow: "auto" }}>
       <ul className="list-group">
         {places.map(place => (
           <li key={place.id} className="list-group-item">
